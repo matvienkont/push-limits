afdc29f762ec073489dbdbfed3c3b4450a2b2b73
Nazar M.T <matvienkont@outlook.com>
2020-08-27 16:48:45 +0300

    configure beta branch

--- android/app/src/main/java/com/limitsbeta/MainApplication.java
+++ android/app/src/main/java/com/limitsbeta/MainApplication.java
@@ -1,4 +1,4 @@
-package com.limits;
+package com.limitsbeta;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.limits.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.limitsbeta.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
