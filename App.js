95e3a6f3af52c6ebf21642321898df9a439f4ffc
Nazar <matvienkont@yahoo.com>
2020-09-13 21:16:58 +0300

    code cleaning 2 App.js

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from "react";
-
-import {
-  StyleSheet,
-  Button
-} from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HabitScreen from "./src/Screens/HabitScreen";
-import HomeScreen from "./src/Screens/HomeScreen";
 import initialiseNotifications from "./src/helpers/notifications/initialiseNotifications";
 import scheduleNotifications from "./src/helpers/notifications/scheduleNotifications";
 
 import PushNotification from "react-native-push-notification";
 import FlashMessage from "react-native-flash-message";
 
-const Stack = createStackNavigator();
+import Navigation from "./src/components/navigation/navigationInit";
+
 
 class App extends React.Component 
 {
@@ -46,63 +37,12 @@ class App extends React.Component
 	render () {
 		return (
 			<>
-			<NavigationContainer>
-				{/*<View style={styles.border}>
-				<StatusBar backgroundColor="#D9CEC1"  barStyle="dark-content" />*/}
-				<Stack.Navigator initialRouteName="Home">
-					<Stack.Screen name="Habit" options={{
-										 	title: 'Habit',
-											headerStyle: {
-												backgroundColor: '#D9CEC1',
-											},
-											headerTitleAlign: "center",	
-											headerTintColor: '#fff',
-											headerStatusBarHeight: 0,
-											headerTitleStyle: {
-												fontWeight: 'bold',
-												fontFamily: "monospace", 
-												fontSize: 20
-											}
-											}}>
-					{ (props) => <HabitScreen {...props} getCounter={this.getCounter.bind(this)} /> }
-					</Stack.Screen> 
-					<Stack.Screen name="Home" options={{
-										 	title: 'Home',
-											headerStyle: {
-												backgroundColor: '#D9CEC1'
-											},
-											headerTitleAlign: "center",	
-											headerTintColor: '#fff',
-											headerStatusBarHeight: 0,
-											headerTitleStyle: {
-												fontWeight: 'bold',
-												fontFamily: "monospace", 
-												fontSize: 20
-											},
-									        headerRight: () => (
-												<Button
-												onPress={() => this.getInputState(true)}
-												title="Info"
-												color="#fff"
-												/>
-											)
-											}}>
-						{(props) => <HomeScreen {...props} />}
-					</Stack.Screen>
-				</Stack.Navigator>
-			</NavigationContainer>
-			<FlashMessage position="top" />	
+				<Navigation />
+				<FlashMessage position="top" />	
 			</>
 		);
 	}
   
 };
-
-const styles = StyleSheet.create({
-	border: {
-		flex:1,
-		backgroundColor: "#D9CEC1",
-	}
-});
 
 export default App;
---
+++ src/components/navigation/navigationInit.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HabitScreen from "../../Screens/HabitScreen";
+import HomeScreen from "../../Screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+export default class Navigation extends React.Component
+{
+    render ()
+    { 
+        return (
+            <NavigationContainer>
+                    <Stack.Navigator initialRouteName="Home">
+                        <Stack.Screen name="Habit" options={{
+                                title: 'Habit',
+                                headerStyle: {
+                                    backgroundColor: '#D9CEC1',
+                                },
+                                headerTitleAlign: "center",	
+                                headerTintColor: '#fff',
+                                headerStatusBarHeight: 0,
+                                headerTitleStyle: {
+                                    fontWeight: 'bold',
+                                    fontFamily: "monospace", 
+                                    fontSize: 20
+                                }
+                                }}>
+                        { (props) => <HabitScreen {...props} getCounter={this.getCounter.bind(this)} /> }
+                        </Stack.Screen> 
+                        <Stack.Screen name="Home" options={{
+                                title: 'Home',
+                                headerStyle: {
+                                    backgroundColor: '#D9CEC1'
+                                },
+                                headerTitleAlign: "center",	
+                                headerTintColor: '#fff',
+                                headerStatusBarHeight: 0,
+                                headerTitleStyle: {
+                                    fontWeight: 'bold',
+                                    fontFamily: "monospace", 
+                                    fontSize: 20
+                                }
+                                }}>
+                            {(props) => <HomeScreen {...props} />}
+                        </Stack.Screen>
+                    </Stack.Navigator>
+                </NavigationContainer>
+        );
+    }
+}
